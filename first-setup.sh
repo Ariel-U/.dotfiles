330ff54644075ee7d7d1c6bd1b686ede8f453ff5
ariel-u <foros.pokerlike@slmail.me>
2026-07-23 01:04:33 -0300

    update

--- first-setup.sh
+++ first-setup.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env bash
 
+## EXPERIMENTAL
+
 ## make backups
 mv -v ~/.config ~/.config.bak && mkdir ~/.config
 mv -v ~/.local ~/.local.bak && mkdir ~/.local
 
-shopt -s dotglob nullglob
-
-dotfiles_dir="$HOME/.dotfiles/home"
-
-for src in "$dotfiles_dir"/*; do
-    [[ -f "$src" ]] || continue
-
-    name=$(basename "$src")
-    dst="$HOME/$name"
-
-    [[ -f "$dst" ]] || continue
-
-    if cmp -s "$src" "$dst"; then
-        echo "Renombrando: $dst -> $dst.bak"
-        mv "$dst" "$dst.bak"
-    fi
-done
+# copy files whith rsync
+rsync -aAXv config/.config/ $HOME/.config/
+rsync -aAXv local/.local/ $HOME/.local/
+rsync -aAXv home// $HOME/
 
 ## use stow to link the dotfiles
-stow config local home
-
+stow --adopt config
+stow --adopt local 
+stow --adopt home
 
 ## restore backups that are not in .dotfiles folder
 rsync -aAXv --ignore-existing --progress ~/.config.bak/ ~/.config/
 rsync -aAXv --ignore-existing --progress ~/.local.bak/ ~/.local/
 
-
 ## remove backups
-rm -rf ~/.config.bak ~/.local.bak
+#rm -rf ~/.config.bak ~/.local.bak
